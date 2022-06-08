6fdbab59dedea8bf3c062dd81447dc552e0dcc6b
Gabriel <ga0301@outlook.com>
2022-06-07 21:30:14 -0300

    feat: file-in-keys

--- generatorKey.js
+++ generatorKey.js
@@ -1,17 +1,39 @@
 const fs = require("fs");
-/*
 const keygen = require("keygenerator");
-const generate = keygen.password({
+
+const keyFacebook = keygen.password({
     length: 15,
     numbers: true,
-}) */
+    upperCase: true,
+});
 
-fs.readFile('./text.txt', "utf8", function (err, data) {
+const keyTwitter = keygen.password({
+    length: 15,
+    numbers: true,
+    upperCase: true,
+});
+
+const keyInstagram = keygen.password({
+    length: 15,
+    numbers: true,
+    upperCase: true,
+})
+
+const currentKey = [
+`Keys -  
+Twitter: ${keyTwitter}
+Facebook: ${keyFacebook}
+Instagram: ${keyInstagram}`
+]
+
+console.log(currentKey);
+
+fs.writeFile('./keys.txt', `${currentKey}`, function (err) {
     if (err) {
         console.log(err);
         return;
     }
     else{
-        console.log(data.toString());
+        console.log("KEYS GENERATED!!!");
     }
 });
